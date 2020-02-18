6be4d199c612d8b3d2ad84b06472fe04be414554
juanrc235 <juan.romero2@alu.uclm.es>
2020-02-18 20:35:39 +0100

    Casi esta pb

--- practica1/src/pb.c
+++ practica1/src/pb.c
@@ -6,9 +6,13 @@
 #include <string.h>
 #include <fcntl.h>
 
-#define PERMISOS 0777
 #define ESPACIO " "
 #define N_BYTES 4096
+#define BARRA "/"
+#define SRC_PATH "modelos/"
+#define EXTENSION ".pdf"
+
+ssize_t getdelim(char **lineptr, size_t *n, int delim, FILE *stream);
 
 void copiar (char *src_path, char *dst_path) {
 
@@ -43,26 +47,53 @@ void copiar (char *src_path, char *dst_path) {
 
 int main(int argc, char const *argv[]) {
 
-  char *nombre_fichero = "estudiantes_p1.text";
   char *line = NULL;
-  char *nombre_dir = NULL, *tipo_ex = NULL;
+  char *nombre_dir = NULL, *tipo_ex = NULL, src_path[256], dst_path[256];
+  char file_name[256];
+  struct stat sb;
 
   size_t len = 0;
   FILE *file;
-  file = fopen(nombre_fichero, "r");
+
+  file = fopen("estudiantes_p1.text", "r");
 
   if (file) {
 
     while (getdelim(&line, &len, '\n', file) != -1) {
       nombre_dir = strtok(line, ESPACIO);
       tipo_ex = strtok(NULL, ESPACIO);
-      printf("%s || %s\n", nombre_dir, tipo_ex);
+
+      if (stat(nombre_dir, &sb) == 0 && S_ISDIR(sb.st_mode)) {
+
+        /* Construimos el nombre del fichero */
+        strcat(file_name, tipo_ex);
+        strcat(file_name, EXTENSION);
+
+        /* Construimos el directorio destino */
+        strcat(dst_path, nombre_dir);
+        strcat(dst_path, BARRA);
+        strcat(dst_path, file_name);
+
+        /* Construimos el directorio origen */
+        strcat(src_path, SRC_PATH);
+        strcat(src_path, file_name);
+
+        printf("%s\n", src_path);
+
+        copiar(src_path, dst_path);
+
+        /* Limpia las variables */
+        memset(&file_name[0], 0, sizeof(file_name));
+        memset(&dst_path[0], 0, sizeof(dst_path));
+        memset(&src_path[0], 0, sizeof(src_path));
+      } else {
+        printf("[PB] El directorio <%s/> no existe\n", nombre_dir);
+      }
+
     }
 
     fclose(file);
   }
-
-  copiar("modelos/MODELOA.pdf", "prueba/a.pdf");
 
   return 0;
 }
