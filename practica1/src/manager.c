62a0650617e7fbb1c3656d31fdc483fb190da09c
juanrc235 <juan.romero2@alu.uclm.es>
2020-02-20 11:21:16 +0100

    terminado funcinalidad base del MANAGER

--- practica1/src/manager.c
+++ practica1/src/manager.c
@@ -4,10 +4,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define NUM_PROC 2
+
 int main(int argc, char const *argv[]) {
 
-  pid_t pa_pid;
-  int status = 0;
+  pid_t pa_pid, pids[NUM_PROC], tmp_pid;
+  int status = 0, i;
 
   pa_pid = fork();
   if (pa_pid == -1) {
@@ -26,7 +28,39 @@ int main(int argc, char const *argv[]) {
     /* limpiar todo */
   }
 
-  printf("[MANAGER] Proceso <pa> finaliza\n");
+  printf("[MANAGER] Proceso [PA] finaliza\n");
+
+  for (i=0; i < NUM_PROC; i++) {
+    if ((pids[i] = fork()) == 0) {
+      switch (i) {
+        case 0:
+          printf("[MANAGER] Creando proceso [PB]\n");
+          execl("./exec/pb", "a", NULL);
+          fprintf(stderr, "[MANAGER] ERROR en execl\n");
+          return EXIT_FAILURE;
+        break;
+        case 1:
+          printf("[MANAGER] Creando proceso [PC]\n");
+          execl("./exec/pc", "a", NULL);
+          fprintf(stderr, "[MANAGER] ERROR en execl\n");
+          return EXIT_FAILURE;
+        break;
+      }
+    } else if(pids[i] > 0) {
+    } else {
+      fprintf(stderr, "[MANAGER] Error creado [PA] y [PC]\n");
+      return EXIT_FAILURE;
+    }
+  }
+
+  for (i = 0; i < NUM_PROC; i++) {
+    tmp_pid = wait(&status);
+    if (tmp_pid == pids[0]) {
+      printf("[MANAGER] Proceso [PB] finaliza\n");
+    } else if (tmp_pid == pids[1]){
+      printf("[MANAGER] Proceso [PC] finaliza\n");
+    }
+  }
 
   return EXIT_SUCCESS;
 }
