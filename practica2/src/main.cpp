9bd9188dc47163d303f9f742a2b2a056ba94abd7
juanrc235 <juan.romero2@alu.uclm.es>
2020-03-17 17:37:04 +0100

    Buscar funciona bien

--- practica2/src/main.cpp
+++ practica2/src/main.cpp
@@ -6,17 +6,21 @@
 
 int main(int argc, char const *argv[]) {
 
+  std::string palabra(argv[1]);
   Resultado resultado_hilo_1 (1, 25, 75);
   std::cout << resultado_hilo_1.devolver_resultado() << std::endl;
 
   std::string line;
   std::ifstream fd ("fichero_casos.txt");
-  std::regex p("casa", std::regex_constants::ECMAScript | std::regex_constants::icase);
+  std::string reg_exp = palabra + "[ .,?!]";
+  std::regex p(reg_exp, std::regex_constants::ECMAScript | std::regex_constants::icase);
+  std::string tmp;
 
   if (fd.is_open()) {
     while ( getline (fd, line) ) {
       if ( std::regex_search(line, p) ) {
-        std::cout << line << std::endl;
+        tmp = std::regex_replace(line, p, "\e[3m$&\e[0m");
+        std::cout << tmp << std::endl;
       }
     }
     fd.close();
